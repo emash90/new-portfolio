62d602bee575eed6f1621d918fe7d33a3ab10257
edwin kairu <edwin.macharia@gozem.co>
2025-04-16 09:36:57 +0300

    fix for vercel deployment

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -5,14 +5,14 @@ export const projects: Project[] = [
   {
     title: 'Car Rental Project',
     description: 'A full stack web application done using AWS amplify with a severless backend utilizing AWS tools such as Lambda, DynamoDB and API Gateway',
-    image: '../public/car rental management pic.avif',
+    image: '/car rental management pic.avif',
     githubUrl: 'https://github.com/emash90/rent-a-car',
     technologies: ['React', 'Lambda', 'S3', 'API Gateway', 'DynamoDB', 'AWS', 'AWS Cognito', 'Amplify'],
   },
   {
     title: 'Tenant Management System: Wama Rentals',
     description: 'A responsive chat app with WebSocket integration.',
-    image: '../public/wama-Screenshot 2025-03-14 at 07.50.46.png',
+    image: '/wama-Screenshot 2025-03-14 at 07.50.46.png',
     githubUrl: 'https://github.com/emash90/wama_microservices',
     technologies: ['React', 'Socket.IO', 'MongoDB', 'Express', 'Tailwind CSS'],
   },
@@ -20,7 +20,7 @@ export const projects: Project[] = [
     title: 'Package Delivery Project: Packaroo',
     description:
       'A loosely coupled delivery system with message brokering and CI/CD automation.',
-    image: '../public/premium_photo-1681487829842-2aeff98f8b63.avif',
+    image: '/premium_photo-1681487829842-2aeff98f8b63.avif',
     githubUrl: 'https://github.com/emash90/package-delivery-app',
     technologies: ['TypeScript', 'MongoDB', 'RabbitMQ', 'GitHub Actions'],
   },
